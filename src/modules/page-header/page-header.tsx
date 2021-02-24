83e44eea74a26d4ee6f6ea24d8df500592021fd2
Maksim Zhbanov <gbanof96@gmail.com>
2021-02-25 00:29:12 +0300

    Поправил кликабельность ссылок в хедере

--- src/modules/page-header/page-header.tsx
+++ src/modules/page-header/page-header.tsx
@@ -1,7 +1,8 @@
 import {Container, Header, Icon, Menu} from "semantic-ui-react";
 import * as React from "react";
-import {Link, withRouter} from "react-router-dom";
+import {useHistory, withRouter} from "react-router-dom";
 import {compose} from "redux";
+import {noop} from "lodash";
 import {TPageHeaderConnectedProps, withPageHeaderConnector} from "./page-header-connector";
 import {THeaderNavItem} from "./page-header-types";
 
@@ -10,39 +11,47 @@ type TProps = TPageHeaderConnectedProps;
 /**
  * Заголовок страницы
  */
-export const PageHeader = React.memo((props: TProps) => (
-    <Menu fixed="top" style={{ height: "5em" }}>
-        <Container>
-            <Menu.Item>
-                <Header>
-                    <Icon name="chess" />
-                    Half Random Tactical Arena
-                </Header>
-            </Menu.Item>
-            {
-                props.headerMenuNavigation.map((item: THeaderNavItem) => (
-                    <Menu.Item as="a" active={item.active}>
-                        {
-                            item.active
-                                ? (
-                                    <Header>
-                                        {item.title}
-                                    </Header>
-                                )
-                                : (
-                                    <Link to={item.link}>
-                                        <Header>
-                                            {item.title}
-                                        </Header>
-                                    </Link>
-                                )
-                        }
-                    </Menu.Item>
-                ))
-            }
-        </Container>
-    </Menu>
-));
+export const PageHeader = React.memo((props: TProps) => {
+    const history = useHistory();
+
+    /**
+     * Обработка нажатия на ячейку навигации хедера
+     */
+    const handleMenuItemClick = (path: string) => () => {
+        history.push(path);
+    }
+
+    return (
+        <Menu fixed="top" style={{ height: "5em" }}>
+            <Container>
+                <Menu.Item>
+                    <Header>
+                        <Icon name="chess" />
+                        Half Random Tactical Arena
+                    </Header>
+                </Menu.Item>
+                {
+                    props.headerMenuNavigation.map((item: THeaderNavItem) => (
+                        <Menu.Item
+                            as="a"
+                            active={item.active}
+                            key={item.link}
+                            onClick={
+                                item.active
+                                    ? noop
+                                    : handleMenuItemClick(item.link)
+                            }
+                        >
+                            <Header>
+                                {item.title}
+                            </Header>
+                        </Menu.Item>
+                    ))
+                }
+            </Container>
+        </Menu>
+    )
+});
 
 export const PageHeaderController = compose<React.FC>(
     withRouter,
