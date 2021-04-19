60214e6714e413eb9654b44a6d7a2c38533d9be5
Maksim Zhbanov <gbanof96@gmail.com>
2021-04-20 00:23:21 +0300

    Поправил basic аутентификацию

--- src/utils/create-request.ts
+++ src/utils/create-request.ts
@@ -7,7 +7,7 @@ const DEVELOP_AXIOS_OPTIONS = {
 const PRODUCTION_AXIOS_OPTIONS = {
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
-        'Authorization': btoa(process.env.REACT_APP_LOGIN + ':' + process.env.REACT_APP_PASSWORD),
+        'Authorization': 'Basic ' + btoa(process.env.REACT_APP_LOGIN + ':' + process.env.REACT_APP_PASSWORD),
     }
 };
 
@@ -15,10 +15,6 @@ const PRODUCTION_AXIOS_OPTIONS = {
  * Конструктор запросов
  */
 export const createRequest = () => {
-    console.log("process.env.REACT_APP_API_URL:", process.env.REACT_APP_API_URL);
-    console.log("process.env.REACT_APP_LOGIN:", process.env.REACT_APP_LOGIN);
-    console.log("process.env.REACT_APP_PASSWORD:", process.env.REACT_APP_PASSWORD);
-
     const option = process.env.NODE_ENV !== 'production'
         ? DEVELOP_AXIOS_OPTIONS
         : PRODUCTION_AXIOS_OPTIONS
