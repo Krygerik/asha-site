8dd69c0dfb8cd870abaf3ce1fb7467bdbaeafea0
Maksim Zhbanov <gbanof96@gmail.com>
2021-05-22 18:20:24 +0300

    Поправил вывод процентов для статистики

--- src/pages/statistics/statistics-page.tsx
+++ src/pages/statistics/statistics-page.tsx
@@ -79,7 +79,7 @@ const StatisticPage = React.memo((props: TStatisticsPageConnectedProps) => {
                                             <Table.Cell>
                                                 <b>
                                                     {
-                                                        countGames && Math.floor(Number(win)/(Number(win) + Number(lose)))
+                                                        countGames && Math.floor(Number(win)/(Number(win) + Number(lose) * 100))
                                                     } %
                                                 </b>
                                                 <br />
