b26f7a4f93f5527c618a800d3d63fd410214948e
Maksim Zhbanov <gbanof96@gmail.com>
2021-05-27 01:39:36 +0300

    Отрефакторил поля фильтров статистики

--- src/pages/statistics/components/race-win-rate-statistics-filter.tsx
+++ src/pages/statistics/components/race-win-rate-statistics-filter.tsx
@@ -1,19 +1,16 @@
+import {map} from "lodash";
 import * as React from "react";
 import {Button, Form as SemanticForm, Grid, Header, Icon, Segment} from "semantic-ui-react";
 import {Form} from "react-final-form";
 import arrayMutators from "final-form-arrays";
 import {FieldArray} from "react-final-form-arrays";
-import {FinalFormInputTextField} from "../../../components/final-form-input-text-field";
 import {FinalFormSelectField} from "../../../components/final-form-select-field";
-import {FinalFormDictionarySelectField} from "../../../components/final-form-dictionary-select-field";
-import {FinalFormUsersSelectField} from "../../../components/final-form-users-select-field";
-import {EDictionaryName} from "../../../modules/dictionary";
-import {EFiltersName, TFilterStatisticsFormValues, TSingleStatisticsFilter} from "../statistics-page-types";
+import {TFilterStatisticsFormValues, TSingleStatisticsFilter} from "../statistics-page-types";
 import {
     COMPARISON_OPERATORS_OPTIONS,
-    FILTERS_LIST,
-    PLAYER_COLOR_OPTIONS,
-    PLAYER_STARTED_BONUS_OPTIONS
+    FILTERS_WITH_COMPARISON_OPERATORS,
+    MAP_FILTERS_NAME_TO_LABEL,
+    MAP_FILTER_NAMES_TO_FIELD_DATA,
 } from "../statistics-page-constants";
 
 type TProps = {
@@ -39,107 +36,67 @@ export const RacesWinRateStatisticsFilter = React.memo((props: TProps) => (
                     <Grid>
                         <FieldArray name="filters">
                             {({ fields }) =>
-                                fields.map((name, index) => (
-                                    <Grid.Row key={name}>
-                                        <Grid.Column width={4}>
-                                            <FinalFormSelectField
-                                                label="Фильтр"
-                                                name={`${name}.name`}
-                                                options={
-                                                    FILTERS_LIST.map(item => ({
-                                                        key: item.name,
-                                                        value: item.name,
-                                                        text: item.label,
-                                                    }))
-                                                }
-                                            />
-                                        </Grid.Column>
-                                        {
-                                            values.filters[index].name === EFiltersName.UserId && (
-                                                <Grid.Column width={10}>
-                                                    <FinalFormUsersSelectField
-                                                        label="Никнейм"
-                                                        name={`${name}.value`}
-                                                    />
-                                                </Grid.Column>
-                                            )
-                                        }
-                                        {
-                                            values.filters[index].name === EFiltersName.Color && (
-                                                <Grid.Column width={10}>
-                                                    <FinalFormSelectField
-                                                        label="Цвет игрока"
-                                                        name={`${name}.value`}
-                                                        options={PLAYER_COLOR_OPTIONS}
-                                                    />
-                                                </Grid.Column>
-                                            )
-                                        }
-                                        {
-                                            values.filters[index].name === EFiltersName.Race && (
-                                                <Grid.Column width={10}>
-                                                    <FinalFormDictionarySelectField
-                                                        dictionary={EDictionaryName.Races}
-                                                        label="Фракция"
-                                                        name={`${name}.value`}
-                                                    />
-                                                </Grid.Column>
-                                            )
-                                        }
-                                        {
-                                            values.filters[index].name === EFiltersName.Hero && (
-                                                <Grid.Column width={10}>
-                                                    <FinalFormDictionarySelectField
-                                                        dictionary={EDictionaryName.Heroes}
-                                                        label="Герой"
-                                                        name={`${name}.value`}
-                                                    />
-                                                </Grid.Column>
-                                            )
-                                        }
-                                        {
-                                            values.filters[index].name === EFiltersName.StartBonus && (
-                                                <Grid.Column width={10}>
-                                                    <FinalFormSelectField
-                                                        label="Стартовый бонус"
-                                                        name={`${name}.value`}
-                                                        options={PLAYER_STARTED_BONUS_OPTIONS}
-                                                    />
-                                                </Grid.Column>
-                                            )
-                                        }
-                                        {
-                                            values.filters[index].name === EFiltersName.Mentoring && (
-                                                <>
-                                                    <Grid.Column width={5}>
-                                                        <FinalFormSelectField
-                                                            label="Сравнение"
-                                                            name={`${name}.operator`}
-                                                            options={COMPARISON_OPERATORS_OPTIONS}
-                                                        />
-                                                    </Grid.Column>
-                                                    <Grid.Column width={5}>
-                                                        <FinalFormInputTextField
-                                                            label="Количество использований ментора"
-                                                            name={`${name}.value`}
-                                                        />
-                                                    </Grid.Column>
-                                                </>
-                                            )
-                                        }
-                                        <Grid.Column width={1} textAlign="center" verticalAlign="middle">
-                                            <Icon
-                                                size="large"
-                                                name="close"
-                                                style={{
-                                                    cursor: "pointer",
-                                                    paddingTop: "10px"
-                                                }}
-                                                onClick={() => fields.remove(index)}
-                                            />
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                ))}
+                                fields.map((name, index) => {
+                                    const selectedFieldName = values.filters[index].name;
+                                    const withComparison = FILTERS_WITH_COMPARISON_OPERATORS.includes(selectedFieldName);
+                                    const {Component, staticProps} = MAP_FILTER_NAMES_TO_FIELD_DATA[selectedFieldName] || {};
+
+                                    return (
+                                        <Grid.Row key={name}>
+                                            <Grid.Column width={4}>
+                                                <FinalFormSelectField
+                                                    label="Фильтр"
+                                                    name={`${name}.name`}
+                                                    options={
+                                                        map(MAP_FILTERS_NAME_TO_LABEL, (value, key) => ({
+                                                            key: key,
+                                                            text: value,
+                                                            value: key,
+                                                        }))
+                                                    }
+                                                />
+                                            </Grid.Column>
+                                            {
+                                                selectedFieldName && (
+                                                    <>
+                                                        {
+                                                            withComparison && (
+                                                                <Grid.Column width={5}>
+                                                                    <FinalFormSelectField
+                                                                        label="Сравнение"
+                                                                        name={`${name}.operator`}
+                                                                        options={COMPARISON_OPERATORS_OPTIONS}
+                                                                    />
+                                                                </Grid.Column>
+                                                            )
+                                                        }
+                                                        <Grid.Column width={withComparison ? 5 : 10}>
+                                                            {
+                                                                // @ts-ignore
+                                                                <Component
+                                                                    {...staticProps}
+                                                                    name={`${name}.value`}
+                                                                />
+                                                            }
+
+                                                        </Grid.Column>
+                                                    </>
+                                                )
+                                            }
+                                            <Grid.Column width={1} textAlign="center" verticalAlign="middle">
+                                                <Icon
+                                                    size="large"
+                                                    name="close"
+                                                    style={{
+                                                        cursor: "pointer",
+                                                        paddingTop: "10px"
+                                                    }}
+                                                    onClick={() => fields.remove(index)}
+                                                />
+                                            </Grid.Column>
+                                        </Grid.Row>
+                                    )
+                                })}
                         </FieldArray>
                         <Grid.Row>
                             <Grid.Column width={4} textAlign="center">
--- src/pages/statistics/statistics-page-constants.ts
+++ src/pages/statistics/statistics-page-constants.ts
@@ -1,35 +1,22 @@
 import {EComparisonNames, EPlayerColor, EPlayerStartedBonus} from "../../common/constants";
+import {FinalFormDictionarySelectField} from "../../components/final-form-dictionary-select-field";
+import {FinalFormInputTextField} from "../../components/final-form-input-text-field";
+import {FinalFormSelectField} from "../../components/final-form-select-field";
+import {FinalFormUsersSelectField} from "../../components/final-form-users-select-field";
+import {EDictionaryName} from "../../modules/dictionary";
 import {EFiltersName} from "./statistics-page-types";
 
 /**
- * Список свойств всех фильтров
+ * Мапа фильтров на их лейблы
  */
-export const FILTERS_LIST = [
-    {
-        name: EFiltersName.UserId,
-        label: "Никнейм"
-    },
-    {
-        name: EFiltersName.Color,
-        label: "Цвет"
-    },
-    {
-        name: EFiltersName.Race,
-        label: "Раса"
-    },
-    {
-        name: EFiltersName.Hero,
-        label: "Герой"
-    },
-    {
-        name: EFiltersName.StartBonus,
-        label: "Стартовый бонус"
-    },
-    {
-        name: EFiltersName.Mentoring,
-        label: "Количество ментора"
-    },
-];
+export const MAP_FILTERS_NAME_TO_LABEL = {
+    [EFiltersName.Color]: "Цвет",
+    [EFiltersName.Hero]: "Герой",
+    [EFiltersName.Mentoring]: "Количество ментора",
+    [EFiltersName.Race]: "Раса",
+    [EFiltersName.StartBonus]: "Стартовый бонус",
+    [EFiltersName.UserId]: "Никнейм",
+};
 
 /**
  * Список названий полей, в которых используется оператор сравнения
@@ -38,7 +25,7 @@ export const FILTERS_WITH_COMPARISON_OPERATORS = [
     EFiltersName.Mentoring,
 ];
 
-    /**
+/**
  * Список опций цвета игрока
  */
 export const PLAYER_COLOR_OPTIONS = [
@@ -96,3 +83,50 @@ export const COMPARISON_OPERATORS_OPTIONS = [
         value: EComparisonNames.LessThen,
     },
 ];
+
+/**
+ * Данные полей фильтра по его названию
+ */
+export const MAP_FILTER_NAMES_TO_FIELD_DATA = {
+    [EFiltersName.Race]: {
+        Component: FinalFormDictionarySelectField,
+        staticProps: {
+            dictionary: EDictionaryName.Races,
+            label: MAP_FILTERS_NAME_TO_LABEL[EFiltersName.Race],
+        }
+    },
+    [EFiltersName.Mentoring]: {
+        Component: FinalFormInputTextField,
+        staticProps: {
+            label: MAP_FILTERS_NAME_TO_LABEL[EFiltersName.Mentoring],
+            type: 'number',
+        }
+    },
+    [EFiltersName.StartBonus]: {
+        Component: FinalFormSelectField,
+        staticProps: {
+            label: MAP_FILTERS_NAME_TO_LABEL[EFiltersName.StartBonus],
+            options: PLAYER_STARTED_BONUS_OPTIONS
+        }
+    },
+    [EFiltersName.Hero]: {
+        Component: FinalFormDictionarySelectField,
+        staticProps: {
+            label: MAP_FILTERS_NAME_TO_LABEL[EFiltersName.Hero],
+            dictionary: EDictionaryName.Heroes,
+        }
+    },
+    [EFiltersName.Color]: {
+        Component: FinalFormSelectField,
+        staticProps: {
+            label: MAP_FILTERS_NAME_TO_LABEL[EFiltersName.Color],
+            options: PLAYER_COLOR_OPTIONS
+        }
+    },
+    [EFiltersName.UserId]: {
+        Component: FinalFormUsersSelectField,
+        staticProps: {
+            label: MAP_FILTERS_NAME_TO_LABEL[EFiltersName.UserId],
+        }
+    },
+}
