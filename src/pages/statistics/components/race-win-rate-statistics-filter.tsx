67af68be1c8edeb417e86753e802139641250520
Maksim Zhbanov <gbanof96@gmail.com>
2021-05-28 01:00:47 +0300

    Исправил запрос статистики при каждом добавлении нового фильтра

--- src/pages/statistics/components/race-win-rate-statistics-filter.tsx
+++ src/pages/statistics/components/race-win-rate-statistics-filter.tsx
@@ -4,8 +4,13 @@ import {Button, Form as SemanticForm, Grid, Header, Icon, Segment} from "semanti
 import {Form} from "react-final-form";
 import arrayMutators from "final-form-arrays";
 import {FieldArray} from "react-final-form-arrays";
+import {EComparisonNames} from "../../../common/constants";
 import {FinalFormSelectField} from "../../../components/final-form-select-field";
-import {TFilterStatisticsFormValues, TSingleStatisticsFilter} from "../statistics-page-types";
+import {
+    TFetchStatisticsRequestFilter,
+    TFilterStatisticsFormValues,
+    TSingleStatisticsFilter
+} from "../statistics-page-types";
 import {
     COMPARISON_OPERATORS_OPTIONS,
     FILTERS_WITH_COMPARISON_OPERATORS,
@@ -14,115 +19,139 @@ import {
 } from "../statistics-page-constants";
 
 type TProps = {
-    handleSubmit: (values: TFilterStatisticsFormValues) => void;
-    initialValues: TSingleStatisticsFilter[];
+    setFilters: (values: TFetchStatisticsRequestFilter) => void;
 };
 
 /**
  * Компонент фильтрации статистики по расам
  */
-export const RacesWinRateStatisticsFilter = React.memo((props: TProps) => (
-    <Segment>
-        <Header
-            textAlign="center"
-            content="Параметры фильтрации"
-        />
-        <Form
-            onSubmit={props.handleSubmit}
-            initialValues={{ filters: props.initialValues }}
-            mutators={{ ...arrayMutators }}
-            render={({ handleSubmit, form: { mutators: { push } }, values }) => (
-                <SemanticForm size='large' onSubmit={handleSubmit}>
-                    <Grid>
-                        <FieldArray name="filters">
-                            {({ fields }) =>
-                                fields.map((name, index) => {
-                                    const selectedFieldName = values.filters[index].name;
-                                    const withComparison = FILTERS_WITH_COMPARISON_OPERATORS.includes(selectedFieldName);
-                                    const {Component, staticProps} = MAP_FILTER_NAMES_TO_FIELD_DATA[selectedFieldName] || {};
+export const RacesWinRateStatisticsFilter = React.memo((props: TProps) => {
+    /**
+     * Обработчик нажатия кнопки "Отфильтровать"
+     */
+    const handleClickFilterButton = (values: TFilterStatisticsFormValues) => () => {
+        props.setFilters(serializeFilterValues(values.filters));
+    }
 
-                                    return (
-                                        <Grid.Row key={name}>
-                                            <Grid.Column width={4}>
-                                                <FinalFormSelectField
-                                                    label="Фильтр"
-                                                    name={`${name}.name`}
-                                                    options={
-                                                        map(MAP_FILTERS_NAME_TO_LABEL, (value, key) => ({
-                                                            key: key,
-                                                            text: value,
-                                                            value: key,
-                                                        }))
-                                                    }
-                                                />
-                                            </Grid.Column>
-                                            {
-                                                selectedFieldName && (
-                                                    <>
-                                                        {
-                                                            withComparison && (
-                                                                <Grid.Column width={5}>
-                                                                    <FinalFormSelectField
-                                                                        label="Сравнение"
-                                                                        name={`${name}.operator`}
-                                                                        options={COMPARISON_OPERATORS_OPTIONS}
-                                                                    />
-                                                                </Grid.Column>
-                                                            )
+    /**
+     * Преобразование данных формы фильтров в фильтр для запроса статистик
+     */
+    const serializeFilterValues = (filterValues: TSingleStatisticsFilter[]) => (
+        filterValues.reduce((acc, item) => {
+            return {
+                ...acc,
+                [item.name]: FILTERS_WITH_COMPARISON_OPERATORS.includes(item.name)
+                    ? item.operator
+                        ? { [item.operator]: item.value }
+                        : { [EComparisonNames.Equal]: item.value }
+                    : item.value
+            }
+        }, {} as TFetchStatisticsRequestFilter)
+    );
+
+    return (
+        <Segment>
+            <Header
+                textAlign="center"
+                content="Параметры фильтрации"
+            />
+            <Form
+                onSubmit={(values: TFilterStatisticsFormValues) => {}}
+                initialValues={{ filters: [{}] }}
+                mutators={{ ...arrayMutators }}
+                render={({ handleSubmit, form: { mutators: { push } }, values }) => (
+                    <SemanticForm size='large' onSubmit={handleSubmit}>
+                        <Grid>
+                            <FieldArray name="filters">
+                                {({ fields }) =>
+                                    fields.map((name, index) => {
+                                        const selectedFieldName = values.filters[index].name;
+                                        const withComparison = FILTERS_WITH_COMPARISON_OPERATORS.includes(selectedFieldName);
+                                        const {Component, staticProps} = MAP_FILTER_NAMES_TO_FIELD_DATA[selectedFieldName] || {};
+
+                                        return (
+                                            <Grid.Row key={name}>
+                                                <Grid.Column width={4}>
+                                                    <FinalFormSelectField
+                                                        label="Фильтр"
+                                                        name={`${name}.name`}
+                                                        options={
+                                                            map(MAP_FILTERS_NAME_TO_LABEL, (value, key) => ({
+                                                                key: key,
+                                                                text: value,
+                                                                value: key,
+                                                            }))
                                                         }
-                                                        <Grid.Column width={withComparison ? 5 : 10}>
+                                                    />
+                                                </Grid.Column>
+                                                {
+                                                    selectedFieldName && (
+                                                        <>
                                                             {
-                                                                // @ts-ignore
-                                                                <Component
-                                                                    {...staticProps}
-                                                                    name={`${name}.value`}
-                                                                />
+                                                                withComparison && (
+                                                                    <Grid.Column width={5}>
+                                                                        <FinalFormSelectField
+                                                                            label="Сравнение"
+                                                                            name={`${name}.operator`}
+                                                                            options={COMPARISON_OPERATORS_OPTIONS}
+                                                                        />
+                                                                    </Grid.Column>
+                                                                )
                                                             }
+                                                            <Grid.Column width={withComparison ? 5 : 10}>
+                                                                {
+                                                                    // @ts-ignore
+                                                                    <Component
+                                                                        {...staticProps}
+                                                                        name={`${name}.value`}
+                                                                    />
+                                                                }
 
-                                                        </Grid.Column>
-                                                    </>
-                                                )
-                                            }
-                                            <Grid.Column width={1} textAlign="center" verticalAlign="middle">
-                                                <Icon
-                                                    size="large"
-                                                    name="close"
-                                                    style={{
-                                                        cursor: "pointer",
-                                                        paddingTop: "10px"
-                                                    }}
-                                                    onClick={() => fields.remove(index)}
-                                                />
-                                            </Grid.Column>
-                                        </Grid.Row>
-                                    )
-                                })}
-                        </FieldArray>
-                        <Grid.Row>
-                            <Grid.Column width={4} textAlign="center">
-                                <Button
-                                    fluid
-                                    primary
-                                    size='large'
-                                    onClick={() => push("filters", {})}
-                                >
-                                    <Icon name="add" />
-                                    Добавить фильтр
-                                </Button>
-                            </Grid.Column>
-                            <Grid.Column width={4}>
-                                <Button
-                                    content="Отфильтровать"
-                                    fluid
-                                    primary
-                                    size='large'
-                                    type="submit"
-                                />
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </SemanticForm>
-            )}
-        />
-    </Segment>
-));
+                                                            </Grid.Column>
+                                                        </>
+                                                    )
+                                                }
+                                                <Grid.Column width={1} textAlign="center" verticalAlign="middle">
+                                                    <Icon
+                                                        size="large"
+                                                        name="close"
+                                                        style={{
+                                                            cursor: "pointer",
+                                                            paddingTop: "10px"
+                                                        }}
+                                                        onClick={() => fields.remove(index)}
+                                                    />
+                                                </Grid.Column>
+                                            </Grid.Row>
+                                        )
+                                    })}
+                            </FieldArray>
+                            <Grid.Row>
+                                <Grid.Column width={4} textAlign="center">
+                                    <Button
+                                        fluid
+                                        onClick={() => push("filters", {})}
+                                        primary
+                                        size='large'
+                                    >
+                                        <Icon name="add" />
+                                        Добавить фильтр
+                                    </Button>
+                                </Grid.Column>
+                                <Grid.Column width={4}>
+                                    <Button
+                                        content="Отфильтровать"
+                                        fluid
+                                        onClick={handleClickFilterButton(values)}
+                                        primary
+                                        size='large'
+                                    />
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </SemanticForm>
+                )}
+            />
+        </Segment>
+    )
+});
--- src/pages/statistics/statistics-page.tsx
+++ src/pages/statistics/statistics-page.tsx
@@ -1,54 +1,26 @@
 import * as React from "react";
 import {Message} from "semantic-ui-react";
-import {EComparisonNames} from "../../common/constants";
 import {Page} from "../../modules/page";
 import {withDictionaries} from "../../modules/dictionary";
 import {
     TFetchStatisticsRequestFilter,
-    TFilterStatisticsFormValues,
-    TSingleStatisticsFilter,
 } from "./statistics-page-types";
 import {RacesWinRateStatisticsFilter} from "./components/race-win-rate-statistics-filter";
 import {RaceWinRateStatisticsResult} from "./components/race-win-rate-statistics-result";
-import {FILTERS_WITH_COMPARISON_OPERATORS} from "./statistics-page-constants";
 
 /**
  * Страница отображения статистик
  */
 const StatisticPage = React.memo(() => {
-    const [savedFilterValues, setFilterSavedValues] = React.useState([{}] as TSingleStatisticsFilter[]);
-
-    /**
-     * Обработчик применения фильтров
-     */
-    const handleSubmitFiltersForm = (values: TFilterStatisticsFormValues) => {
-        setFilterSavedValues(values.filters);
-    }
-
-    /**
-     * Преобразование данных формы фильтров в фильтр для запроса статистик
-     */
-    const serializeFilterValues = (filterValues: TSingleStatisticsFilter[]) => (
-        filterValues.reduce((acc, item) => {
-            return {
-                ...acc,
-                [item.name]: FILTERS_WITH_COMPARISON_OPERATORS.includes(item.name)
-                    ? item.operator
-                        ? { [item.operator]: item.value }
-                        : { [EComparisonNames.Equal]: item.value }
-                    : item.value
-            }
-        }, {} as TFetchStatisticsRequestFilter)
-    );
+    const [filters, setFilters] = React.useState({} as TFetchStatisticsRequestFilter);
 
     return (
         <Page>
             <RacesWinRateStatisticsFilter
-                handleSubmit={handleSubmitFiltersForm}
-                initialValues={savedFilterValues}
+                setFilters={setFilters}
             />
             <RaceWinRateStatisticsResult
-                filters={serializeFilterValues(savedFilterValues)}
+                filters={filters}
             />
             <Message style={{ fontSize: '18px' }}>
                 <Message.Header content="Подсказки" />
